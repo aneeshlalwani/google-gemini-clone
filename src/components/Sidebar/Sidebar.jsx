e7eaad11b9f8a12aa84455719e458f279c1e7277
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-14 14:27:46 +0500

    implemented sidebar toggle functionality

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,46 @@
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [sidebarExtended, setSidebarExtended] = useState(false);
+
   return (
     <>
       <div className="sidebar">
         <div className="top">
-          <img className="menu" src={assets.menu_icon} alt="MenuIcon" />
+          <img
+            className="menu"
+            src={assets.menu_icon}
+            alt="MenuIcon"
+            onClick={() => setSidebarExtended((previous) => !previous)}
+          />
           <div className="new-chat">
-            <img src={assets.plus_icon} alt="PlusIcon" />
-            <p>New Chat</p>
+            <img src={assets.plus_icon} alt="" />
+            {sidebarExtended ? <p>New Chat</p> : null}
           </div>
-          <div className="recent">
-            <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="MessageIcon" />
-              <p>What is react...</p>
+          {sidebarExtended ? (
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entry">
+                <img src={assets.message_icon} alt="MessageIcon" />
+                <p>What is react...</p>
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
         <div className="bottom">
           <div className="bottom-item recent-entry">
             <img src={assets.question_icon} alt="QuestionIcon" />
-            <p>Help</p>
+            {sidebarExtended ? <p>Help</p> : null}
           </div>
           <div className="bottom-item recent-entry">
             <img src={assets.history_icon} alt="HistoryIcon" />
-            <p>Activity</p>
+            {sidebarExtended ? <p>Activity</p> : null}
           </div>
           <div className="bottom-item recent-entry">
             <img src={assets.setting_icon} alt="SettingIcon" />
-            <p>Settings</p>
+            {sidebarExtended ? <p>Settings</p> : null}
           </div>
         </div>
       </div>
