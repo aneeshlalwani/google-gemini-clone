fdf405febdbf137f49d59b4a6f8009d56448f888
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-16 01:06:03 +0500

    implemented main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,60 @@
+import "./Main.css";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+          <p>Gemini</p>
+          <img src={assets.user_icon} alt="UserIcon" />
+        </div>
+        <div className="main-container">
+          <div className="greet">
+            <p>
+              <span>Hello, Aneesh</span>
+            </p>
+            <p>How can I help you today?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Suggest beautiful places to see on upcoming road trip</p>
+              <img src={assets.compass_icon} alt="CompassIcon" />
+            </div>
+            <div className="card">
+              <p>Suggest beautiful places to see on upcoming road trip</p>
+              <img src={assets.bulb_icon} alt="CompassIcon" />
+            </div>
+            <div className="card">
+              <p>Suggest beautiful places to see on upcoming road trip</p>
+              <img src={assets.message_icon} alt="CompassIcon" />
+            </div>
+            <div className="card">
+              <p>Suggest beautiful places to see on upcoming road trip</p>
+              <img src={assets.code_icon} alt="CompassIcon" />
+            </div>
+          </div>
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder="Enter a prompt here" />
+              <div className="search-box-icon">
+                <img src={assets.gallery_icon} alt="GalleryIcon" />
+                <img src={assets.mic_icon} alt="MicIcon" />
+                <img src={assets.send_icon} alt="SendIcon" />
+              </div>
+            </div>
+            <p className="bottom-info">
+              Gemini may display inaccurate info, including about people, so
+              double-check its responses.{" "}
+              <a href="https://support.google.com/gemini/answer/13594961?visit_id=638488069169109558-2959892032&p=privacy_notice&rd=1#privacy_notice">
+                Your privacy & Gemini Apps
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
