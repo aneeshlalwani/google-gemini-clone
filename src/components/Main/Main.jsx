340c021b244a01e6e6d030d9d8d9c0af3da5e2b0
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-17 22:22:39 +0500

    utilized the integrated api to diplay text on web page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,19 @@
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const Main = () => {
+  // destructuring the object of context
+  const {
+    input,
+    setInput,
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+  } = useContext(Context);
   return (
     <>
       <div className="main">
@@ -10,37 +22,62 @@ const Main = () => {
           <img src={assets.user_icon} alt="UserIcon" />
         </div>
         <div className="main-container">
-          <div className="greet">
-            <p>
-              <span>Hello, Aneesh</span>
-            </p>
-            <p>How can I help you today?</p>
-          </div>
-          <div className="cards">
-            <div className="card">
-              <p>Suggest beautiful places to see on upcoming road trip</p>
-              <img src={assets.compass_icon} alt="CompassIcon" />
+          {!showResult ? (
+            <>
+              <div className="greet">
+                <p>
+                  <span>Hello, Aneesh</span>
+                </p>
+                <p>How can I help you today?</p>
+              </div>
+              <div className="cards">
+                <div className="card">
+                  <p>Suggest beautiful places to see on upcoming road trip</p>
+                  <img src={assets.compass_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>Suggest beautiful places to see on upcoming road trip</p>
+                  <img src={assets.bulb_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>Suggest beautiful places to see on upcoming road trip</p>
+                  <img src={assets.message_icon} alt="CompassIcon" />
+                </div>
+                <div className="card">
+                  <p>Suggest beautiful places to see on upcoming road trip</p>
+                  <img src={assets.code_icon} alt="CompassIcon" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="result">
+              <div className="result-title">
+                <img src={assets.user_icon} alt="UserIcon" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="GeminiIcon" />
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              </div>
             </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on upcoming road trip</p>
-              <img src={assets.bulb_icon} alt="CompassIcon" />
-            </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on upcoming road trip</p>
-              <img src={assets.message_icon} alt="CompassIcon" />
-            </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on upcoming road trip</p>
-              <img src={assets.code_icon} alt="CompassIcon" />
-            </div>
-          </div>
+          )}
+
           <div className="main-bottom">
             <div className="search-box">
-              <input type="text" placeholder="Enter a prompt here" />
+              <input
+                onChange={(event) => setInput(event.target.value)}
+                value={input}
+                type="text"
+                placeholder="Enter a prompt here"
+              />
               <div className="search-box-icon">
                 <img src={assets.gallery_icon} alt="GalleryIcon" />
                 <img src={assets.mic_icon} alt="MicIcon" />
-                <img src={assets.send_icon} alt="SendIcon" />
+                <img
+                  onClick={() => onSent()}
+                  src={assets.send_icon}
+                  alt="SendIcon"
+                />
               </div>
             </div>
             <p className="bottom-info">
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,17 +1,46 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-  const onSent = async (prompt) => {
-    // triggers the execution of runChat with provided prompt
-    await runChat(prompt);
-  };
-  // Calls onSent immediately with prompt when ContextProvider component is rendered
-  onSent("What is node.js?");
+  const [input, setInput] = useState("");
+  // state for storing the recent prompts data
+  const [recentPrompt, setRecentPrompt] = useState("");
+  // state for storing the history of prompts data
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  //
+  const [showResult, setShowResult] = useState(false);
+  // for loading
+  const [loading, setLoading] = useState(false);
+  // state for displaying the result on web page
+  const [resultData, setResultData] = useState("");
 
-  const contextValue = {};
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await runChat(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+  /* Calls onSent immediately with prompt when ContextProvider component is rendered */
+  // onSent("What is node.js?");
+
+  const contextValue = {
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompt,
+    setPrevPrompt,
+    showResult,
+    loading,
+    resultData,
+    onSent,
+  };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
