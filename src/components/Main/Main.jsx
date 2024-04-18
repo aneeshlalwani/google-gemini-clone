bcc046e18e0e01a3d541cb06709c69927615e4a1
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-04-18 10:00:43 +0500

    implemented pre loader animation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -57,7 +57,15 @@ const Main = () => {
               </div>
               <div className="result-data">
                 <img src={assets.gemini_icon} alt="GeminiIcon" />
-                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                {loading ? (
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                ) : (
+                  <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                )}
               </div>
             </div>
           )}
